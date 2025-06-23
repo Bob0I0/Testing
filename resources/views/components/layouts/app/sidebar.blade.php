d69b89356de9e0dab77fc41f9a6057f7cd054f46
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2025-06-23 15:25:30 +0700

    Gambar dashboard

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -6,13 +6,14 @@
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
-
-            <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
-                <x-app-logo />
-            </a>
+            <div class="text-center mb-0.5">
+                <img src="{{ asset('images/LOGO-PMI-png-1 2.png') }}" alt="Logo" class="mx-auto mb-0.5 rounded-xl" style="width: 210px; height: 142px; object-fit: contain;">
+                <hr class="mt-1 mb-0 border-zinc-300 dark:border-zinc-600 w-7/8 mx-auto">
+            </div>
 
             <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')" class="grid">
+                <flux:navlist.group class="grid">
+                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
