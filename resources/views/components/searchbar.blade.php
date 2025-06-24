86ac65e9a658800be47e949dc599c9f46566ee8d
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2025-06-24 10:30:01 +0700

    sejajarkan searchbar

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,6 +1,6 @@
-<div class="relative flex-1 me-4">
-    <flux:input variant="filled" placeholder="Search..." icon="magnifying-glass" class="w-full h-9 text-lg" />
-    <span class="absolute inset-y-0 right-2 flex items-center">
-        <flux:avatar badge badge:color="green" circle src="https://unavatar.io/x/calebporzio" class="h-8 w-8" />
-    </span>
+<div class="relative flex-1 me-4 ms-2">
+  <flux:input variant="filled" placeholder="Search..." icon="magnifying-glass" class="w-full h- text-lg" />
+  <span class="absolute inset-y-0 right-2 flex items-center">
+    <flux:avatar badge badge:color="green" circle src="https://unavatar.io/x/calebporzio" class="h-8 w-8" />
+  </span>
 </div>
